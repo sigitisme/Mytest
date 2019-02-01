a8584330f36c56a87799881251b4612512ef1c76
sigitisme <sigitkurniawanisme@gmail.com>
2019-02-01 20:47:06 +0700

    update sample.java with syso only

--- sample.java
+++ sample.java
@@ -5,5 +5,6 @@ public class Sample{
   public static void main(String[] args){
    System.out.println("Hello World Thank You");
    System.out.println("Hello World Thank You");
+   System.out.println("A");
   }
 }
