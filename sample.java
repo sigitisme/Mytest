60ddbc05ebb88776e4c34cf49f31d0fbe6141223
sigit_kurniawan <root@instance-7.us-east1-b.c.plucky-shore-220323.internal>
2019-02-01 13:30:05 +0000

    update readme and sample.java

--- sample.java
+++ sample.java
@@ -3,6 +3,6 @@ import java.*;
 public class Sample{
 
   public static void main(String[] args){
-   System.out.println("Hello World Thanks");
+   System.out.println("Hello World Thank You");
   }
 }
